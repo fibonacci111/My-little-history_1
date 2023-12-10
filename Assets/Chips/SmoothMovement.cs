8d0a4af66a34b616accc2dc3d5e7e6318a587bbf
Поповичев Артём Алексеевич <67004012+ppppppiP@users.noreply.github.com>
2023-12-10 23:31:33 +0300

    sdfg

--- Assets/Chips/SmoothMovement.cs
+++ Assets/Chips/SmoothMovement.cs
@@ -6,10 +6,23 @@ public class SmoothMovement : MonoBehaviour
 {
     [SerializeField] float speed = 5f;
     [SerializeField] Transform targetPosition;
-
+    [SerializeField] float LifeTime;
+    private float time = 0;
+    private void FixedUpdate()
+    {
+        if (time <= LifeTime) {
+            time += 1f;
+        }
+        else
+        {
+            gameObject.SetActive(false);
+            time = 0;
+        }
+    }
     void Update()
     {
         float step = speed * Time.deltaTime;
         transform.position = Vector3.MoveTowards(transform.position, targetPosition.position, step);
     }
+    
 }
